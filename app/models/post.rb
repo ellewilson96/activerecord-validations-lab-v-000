39db80530dc3428109703041204ca76d1a31c1dd
Elle Wilson <eleanorwilsonn@gmail.com>
2018-07-10 15:57:47 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,6 @@ class Post < ActiveRecord::Base
   validates :summary, length: {maximum: 250}
   validates :category, inclusion: {in: %w(Fiction Non-Fiction),
     message: "%{value} is not a valid category"}
+  validates :title, inclusion: {in: %w("Won't Believe" "Secret" "Top [number]" "Guess")
+    message: "false"}
 end
