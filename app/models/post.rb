002665ba71d425265f807ddd5bb3401a732d4dfe
Elle Wilson <eleanorwilsonn@gmail.com>
2018-07-10 16:00:58 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,18 @@ class Post < ActiveRecord::Base
   validates :summary, length: {maximum: 250}
   validates :category, inclusion: {in: %w(Fiction Non-Fiction),
     message: "%{value} is not a valid category"}
-  validates :title, inclusion: {in: %w("Won't Believe" "Secret" "Top [number]" "Guess")
-    message: "false"}
+  validate :is_clickbait?
+
+      CLICKBAIT_PATTERNS = [
+        /Won't Believe/i,
+        /Secret/i,
+        /Top [0-9]*/i,
+        /Guess/i
+      ]
+
+      def is_clickbait?
+        if CLICKBAIT_PATTERNS.none? { |pat| pat.match title }
+          errors.add(:title, "must be clickbait")
+        end
+      end
 end
